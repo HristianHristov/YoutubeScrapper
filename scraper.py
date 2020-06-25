95e10cc4f69afe2aef5975b606c2f6ac8ea35b4f
Hristian Hristov <hristianhristov@localhost.localdomain>
2020-06-25 17:42:10 +0300

    Added NoneType handling. Code refactoring

--- scraper.py
+++ scraper.py
@@ -21,33 +21,33 @@ def main() :
 def scrape(link) :
     video = {}
     source = get_divs(link)
+    print(source)
     video['Title'] = get_title(source)
     video['Link'] = link
     video['Channel'] = channel_info(source)['Channel_name']
     video['Channel_link'] = channel_info(source)['Channel_link']
     video['Channel_subscribers'] = channel_info(source)['Subscribers']
-    video['Category'] = get_category(source)
     video['Views'] = get_views(source)
     video['Likes'] = get_likes(source)
-    video['Related_vids']= get_related_videos(source)
+    video['Related_vids'] = get_related_videos(source)
     return video
 
 def get_divs(link) :
     source = requests.get(link).text
-    soup=BeautifulSoup(source,'lxml')
+    soup = BeautifulSoup(source,'lxml')
     div_s = soup.findAll('div')
     return div_s
 
 def get_title(div_s) :
-    title = div_s[1].find('span',class_='watch-title').text.strip()
+    title = strip(div_s[1].find('span',class_ ='watch-title'))
     return title
 
 def channel_info(div_s) :
-    channel_name = div_s[1].find('a',class_="yt-uix-sessionlink spf-link").text.strip()
-    channel_link = ('www.youtube.com'+div_s[1].find('a',class_="yt-uix-sessionlink spf-link").get('href'))
-    subscribers = div_s[1].find('span',class_="yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count").text.strip()
-    if len(channel_name) ==0 :
-        channel_name ='None'
+    channel_name = strip(div_s[1].find('a',class_ = "yt-uix-sessionlink spf-link"))
+    channel_link = ('www.youtube.com' + get_href(div_s[1].find('a',class_="yt-uix-sessionlink spf-link")))
+    subscribers = strip(div_s[1].find('span',class_ = "yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count"))
+    if len(channel_name) == 0 :
+        channel_name = 'None'
         channel_link = 'None'
         subscribers = 'None'
     channel_info = dict(Channel_name = channel_name,
@@ -55,32 +55,44 @@ def channel_info(div_s) :
                         Subscribers = subscribers)
     return channel_info
 
-def get_category(div_s) :
-    category = div_s[1].findAll('a',class_="yt-uix-sessionlink spf-link")[1].text.strip()
-    return category
-
 def get_views(div_s) :
-    view_count = div_s[1].find('div',class_= 'watch-view-count')
-    view_count = view_count.text[:-11].replace(",", "")
+    try:
+        view_count = div_s[1].find('div',class_ = 'watch-view-count').text[:-11].replace(",", "")
+    except AttributeError as e :
+        return 'NaN'
     return view_count
 
 def get_likes(div_s) :
-    likes = div_s[1].find('button',class_="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-like-button like-button-renderer-like-button-unclicked yt-uix-clickcard-target yt-uix-tooltip" ).text.strip()
+    likes = strip(div_s[1].find('button',class_ = "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-like-button like-button-renderer-like-button-unclicked yt-uix-clickcard-target yt-uix-tooltip"))
     return likes
 
 def get_dislikes(div_s) :
-    dislikes = div_s[1].find('button',class_="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-dislike-button like-button-renderer-dislike-button-unclicked yt-uix-clickcard-target yt-uix-tooltip" ).text.strip()
+    dislikes = strip(div_s[1].find('button',class_ = "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-dislike-button like-button-renderer-dislike-button-unclicked yt-uix-clickcard-target yt-uix-tooltip" ))
     return dislikes
 
 def get_related_videos(div_s) :
-    related_videos = div_s[1].findAll('a',class_='content-link spf-link yt-uix-sessionlink spf-link')
+    related_videos = div_s[1].findAll('a',class_ = 'content-link spf-link yt-uix-sessionlink spf-link')
     title_related = []
     link_related = []
     for i in range(len(related_videos)):
         title_related.append(related_videos[i].get('title'))
-        link_related.append(related_videos[i].get('href'))
+        link_related.append("www.youtube.com" + get_href(related_videos[i]))
     related_dictionary = dict(zip(title_related, link_related))
     return related_dictionary
 
-if __name__== "__main__":
+def strip(data) :
+    try :
+       stripped = data.text.strip()
+    except AttributeError as e:
+        return 'NaN'
+    return stripped
+
+def get_href(data) :
+    try :
+       href = data.get('href')
+    except AttributeError as e:
+        return 'NaN'
+    return href    
+
+if __name__ == "__main__":
   main()
